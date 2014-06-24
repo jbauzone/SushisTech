9fcf4ff1d42b8633093ce0eea508bdfdcf5caaf3
Jérémy Bauzone <bauzonejeremy@gmail.com>
2014-06-24 19:25:18 +0200

    Ajout des librairies de base (bootstrap, jquery*, entity)

---
+++ Eutech.SushisTech/Eutech.SushisTech.Website/App_Start/BundleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Eutech.SushisTech.Website {
+
+    public class BundleConfig {
+
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles) {
+
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.validate.js")
+                .Include("~/Scripts/jquery.validate.unobtrusive.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/bootstrap-theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/Site.css"));
+        }
+        
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
+
+            if (ignoreList == null)
+                throw new ArgumentNullException("ignoreList");
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
+    }
+}
