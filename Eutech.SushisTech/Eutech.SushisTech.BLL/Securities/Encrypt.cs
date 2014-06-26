d99f0d74c2a52ccac69687678d7a286a123d7206
Jérémy Bauzone <bauzonejeremy@gmail.com>
2014-06-26 21:45:17 +0200

    Ajout de méthodes de chiffrement de données basées sur DES

--- Eutech.SushisTech/Eutech.SushisTech.BLL/Securities/Encrypt.cs
+++ Eutech.SushisTech/Eutech.SushisTech.BLL/Securities/Encrypt.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -25,5 +27,93 @@ namespace Eutech.SushisTech.BLL.Securities {
 
             return hashData;
         }
+
+        /// <summary>
+        /// Permet de convertir une chaine dans un tableau de byte
+        /// </summary>
+        /// <param name="strInput">La chaine</param>
+        /// <returns></returns>
+        private static byte[] ConvertToByteArray(string strInput) {
+
+            int intCounter; 
+            char[] arrChar = strInput.ToCharArray();
+
+            byte[] arrByte = new byte[arrChar.Length];
+
+            for (intCounter = 0; intCounter < arrByte.Length; intCounter++)
+                arrByte[intCounter] = Convert.ToByte(arrChar[intCounter]);
+
+            return arrByte;
+        }
+
+        /// <summary>
+        /// Chiffre une chaine selon la clé fournie
+        /// </summary>
+        /// <param name="value">La chaine à chiffrer</param>
+        /// <param name="keyValue">La clé à utiliser pour chiffrer</param>
+        /// <param name="keyIV">Vecteur d'initialisation</param>
+        /// <returns></returns>
+        public static string EncryptString(string value, string keyValue, string keyIV) {
+            
+            byte[] key = ConvertToByteArray(keyValue);
+            byte[] IV = ConvertToByteArray(keyIV);
+
+            byte[] inputByteArray = Encoding.UTF8.GetBytes(value);
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            MemoryStream ms = new MemoryStream(); 
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(key, IV), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+
+            cs.FlushFinalBlock();
+
+            return Convert.ToBase64String(ms.ToArray());
+		}
+
+        /// <summary>
+        /// Déchiffre une chaine selon la clé fournie
+        /// </summary>
+        /// <param name="value">La chaine à déchiffrer</param>
+        /// <param name="keyValue">La clé à utiliser pour déchiffrer</param>
+        /// <param name="keyIV">Vecteur d'initialisation</param>
+        /// <returns></returns>
+		public static string DecryptString(string value, string keyValue, string keyIV) {
+            
+            byte[] key = ConvertToByteArray(keyValue);
+            byte[] IV = ConvertToByteArray(keyIV);
+
+            int len = value.Length;
+            byte[] inputByteArray = Convert.FromBase64String(value);
+
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            MemoryStream ms = new MemoryStream();
+
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(key, IV), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+
+            cs.FlushFinalBlock();
+
+            Encoding encoding = Encoding.UTF8; 
+            return encoding.GetString(ms.ToArray());
+		}
+
+        /// <summary>
+        /// Chiffre une chaine avec la clé par défaut du fichier de configuration
+        /// </summary>
+        /// <param name="value">La chaine à chiffrer</param>
+        /// <returns></returns>
+		public static string EncryptString(string value) {
+            return EncryptString(value, ConfigurationManager.AppSettings["keyValue"], ConfigurationManager.AppSettings["keyIV"]);
+		}
+
+        /// <summary>
+        /// Déchiffre une chaine chiffrée avec la clé par défaut du fichier de configuration
+        /// </summary>
+        /// <param name="value">La chaine à déchiffrer</param>
+        /// <returns></returns>
+		public static string DecryptString(string value) {
+            return DecryptString(value, ConfigurationManager.AppSettings["keyValue"], ConfigurationManager.AppSettings["keyIV"]);
+		}
     }
 }
--- Eutech.SushisTech/Eutech.SushisTech.Tests/Securities/SecurityTest.cs
+++ Eutech.SushisTech/Eutech.SushisTech.Tests/Securities/SecurityTest.cs
@@ -7,6 +7,10 @@ namespace Eutech.SushisTech.Tests.Securities {
     [TestClass]
     public class SecurityTest {
 
+        //clé de test utilisée pour les tests de chiffrement
+        private const string _keyValue = "19iz12da";
+        private const string _ivValue = "zh9ka769";
+
         /// <summary>
         /// Vérifie la validité d'un hash
         /// </summary>
@@ -17,6 +21,30 @@ namespace Eutech.SushisTech.Tests.Securities {
             string expected = "5B41653D572AED6A63BE166359061B83CC06EC9E";
 
             Assert.AreEqual(expected, Encrypt.GetHash(data));
+        }
+        
+        /// <summary>
+        /// Vérifie le chiffrement
+        /// </summary>
+        [TestMethod]
+        public void EncryptionValidTest() {
+
+            string data = "chaîneenclair";
+            string expected = "wApZF8oEcEZdlmxI4MVBDQ==";
+
+            Assert.AreEqual(expected, Encrypt.EncryptString(data, _keyValue, _ivValue));
+        }
+        
+        /// <summary>
+        /// Vérifie le déchiffrement
+        /// </summary>
+        [TestMethod]
+        public void DecryptionValidTest() {
+
+            string data = "wApZF8oEcEZdlmxI4MVBDQ==";
+            string expected = "chaîneenclair";
+
+            Assert.AreEqual(expected, Encrypt.DecryptString(data, _keyValue, _ivValue));
         }
     }
 }
